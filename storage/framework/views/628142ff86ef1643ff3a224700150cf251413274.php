14c2280de4014dcd92a1ebafd5b062e5b1df28a1
Kevin <k3viin.pe@gmail.com>
2022-04-25 15:35:39 +0200

    se cambiaron, agregaron las imagenes y pdf de los folletos en la seccion de descargas

---
+++ storage/framework/views/628142ff86ef1643ff3a224700150cf251413274.php
@@ -0,0 +1,85 @@
+<div class="container-fluid bg-migas size-13">
+    <div class="container">
+        <nav aria-label="breadcrumb bg-migas">
+            <ol class="breadcrumb m-0 p-2">        
+
+                <li class="breadcrumb-item">
+                    <a href="<?php echo e(url('/')); ?>" class="text-decoration-none link-camino-migas">
+                        INICIO
+                    </a>
+                </li>         
+                
+                <?php if(isset($articulo->familia)): ?>
+                    <?php
+                        $familia_actual = $articulo->familia;
+                    ?>
+                <?php endif; ?>
+                
+
+                <?php if(isset($familia_actual)): ?>
+
+                    <li class="breadcrumb-item">
+                        <a href="<?php echo e(url('/nuestros-productos')); ?>" class="text-decoration-none link-camino-migas">
+                            FAMILIAS 
+                        </a>
+                    </li> 
+
+                    <?php if(isset($familia_actual->padre->padre)): ?>
+                        <li class="breadcrumb-item">
+                            <a href="<?php echo e(url('familia/'.$familia_actual->padre->padre->slug.'/categorias')); ?>" class="text-decoration-none link-camino-migas">
+                                <?php echo e($familia_actual->padre->padre->nomfam); ?>
+                            </a>
+                        </li> 
+                    <?php endif; ?> 
+
+                    <?php if($familia_actual->padre): ?>
+                        <li class="breadcrumb-item">
+                            <a href="<?php echo e(url('familia/'.$familia_actual->padre->slug.'/categorias')); ?>" class="text-decoration-none link-camino-migas">
+                                <?php echo e($familia_actual->padre->nomfam); ?>
+                            </a>
+                        </li>          
+                    <?php endif; ?>    
+
+                    <?php if(isset($articulo->familia) ): ?>
+
+                        <li class="breadcrumb-item">
+                            <a href="<?php echo e(url('familia/'.$articulo->familia->slug.'/articulos')); ?>" class="text-decoration-none link-camino-migas">
+                                <?php echo e($articulo->familia->nomfam); ?>
+                            </a>
+                        </li>    
+
+                        <li class="breadcrumb-item text-uppercase" data-toggle="tooltip" data-placement="bottom" title="<?php echo e($articulo->nombre); ?>">
+                            <?php
+                                if (strlen($articulo->nombre) > 30) {
+                                    $nombre_articulo = substr($articulo->nombre,0,30).'...';
+                                }else{
+                                    $nombre_articulo = $articulo->nombre;
+                                }                                               
+                            ?>
+
+                            <?php echo e($nombre_articulo); ?> 
+                        </li>   
+                        
+                    <?php else: ?>
+                    
+                        <li class="breadcrumb-item">
+                            <?php echo e($familia_actual->nomfam); ?>
+                        </li> 
+
+                    <?php endif; ?>        
+                
+                    
+
+                <?php else: ?>
+
+                    <li class="breadcrumb-item">
+                        FAMILIAS
+                    </li>   
+
+                <?php endif; ?>  
+
+            </ol>
+        </nav>
+
+    </div>
+</div><?php /**PATH C:\laragon\www\SPMiguel\resources\views/comun/front/camino-migas.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/9328c35ad06a9a19e340a8c05dbff5ea1741478b.php
@@ -0,0 +1,166 @@
+<?php $__env->startSection('cuerpo'); ?>
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-12 text-center py-3 bd-title">POLITICA DE COOKIES</div>
+
+    </div>
+
+    <div>
+        <p><b>INFORMACION SOBRE COOKIES</b></p>
+        <p>
+            Conforme con la Ley 34/2002, de 11 de julio, de servicios de la sociedad de la información y de comercio
+            electrónico (LSSI), en relación con el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de
+            27 de abril de 2016, General de Protección de Datos (GDPR) y la Ley Orgánica 3/2018, de 5 de diciembre,
+            de Protección de Datos y Garantía de los Derechos Digitales (LOPDGDD), es obligado obtener el
+            consentimiento expreso del usuario de todas las páginas web que usan cookies prescindibles, antes de que
+            este navegue por ellas.
+        </p>
+    </div>
+    <div>
+        <p><b>¿QUÉ SON LAS COOKIES?</b></p>
+        <p>
+            Las cookies y otras tecnologías similares tales como local shared objects, flash cookies o píxeles, son
+            herramientas empleadas por los servidores Web para almacenar y recuperar información acerca de sus
+            visitantes, así como para ofrecer un correcto funcionamiento del sitio. <br>
+            Mediante el uso de estos dispositivos se permite al servidor Web recordar algunos datos concernientes al
+            usuario, como sus preferencias para la visualización de las páginas de ese servidor, nombre y contraseña,
+            productos que más le interesan, etc.
+        </p>
+    </div>
+
+    <div>
+        <p><b>AFECTADAS POR LA NORMATIVA Y COOKIES EXCEPTUADAS</b></p>
+        <p>
+            Según la directiva de la UE, las cookies que requieren el consentimiento informado por parte del usuario
+            son las cookies de analítica y las de publicidad y afiliación, quedando exceptuadas las de carácter técnico y
+            las necesarias para el funcionamiento del sitio web o la prestación de servicios expresamente solicitados por
+            el usuario.
+        </p>
+    </div>
+
+    <div>
+        <p><b>TIPOS DE COOKIES</b></p>
+        <P>SEGÚN LA FINALIDAD</P>
+        <ul>
+            <li>
+                <b>Cookies técnicas y funcionales:</b> son aquellas que permiten al usuario la navegación a través de
+                una página web, plataforma o aplicación y la utilización de las diferentes opciones o servicios que en
+                ella existan.
+            </li>
+            <li>
+                <b>Cookies analíticas:</b> son aquellas que permiten al responsable de las mismas el seguimiento y
+                análisis del comportamiento de los usuarios de los sitios web a los que están vinculadas. La
+                información recogida mediante este tipo de cookies se utiliza en la medición de la actividad de los
+                sitios web, aplicación o plataforma y para la elaboración de perfiles de navegación de los usuarios de
+                dichos sitios, aplicaciones y plataformas, con el fin de introducir mejoras en función del análisis de
+                los datos de uso que hacen los usuarios del servicio.
+            </li>
+            <li>
+                <b>Cookies publicitarias:</b> son aquellas que permiten la gestión, de la forma más eficaz posible, de los
+                espacios publicitarios que, en su caso, el editor haya incluido en una página web, aplicación o
+                plataforma desde la que presta el servicio solicitado en base a criterios como el contenido editado o
+                la frecuencia en la que se muestran los anuncios.
+            </li>
+            <li>
+                <b>Cookies de publicidad comportamental:</b> recogen información sobre las preferencias y elecciones personales del usuario (retargeting) para permitir la gestión, de la forma más eficaz posible, de los
+                espacios publicitarios que, en su caso, el editor haya incluido en una página web, aplicación o
+                plataforma desde la que presta el servicio solicitado.
+            </li>
+            <li>
+                <b>Cookies sociales:</b> son establecidas por las plataformas de redes sociales en los servicios para
+                permitirle compartir contenido con sus amigos y redes. Las plataformas de medios sociales tienen la
+                capacidad de rastrear su actividad en línea fuera de los Servicios. Esto puede afectar al contenido y
+                los mensajes que ve en otros servicios que visita.
+            </li>
+            <li>
+                <b>Cookies de afiliados:</b> permiten hacer un seguimiento de las visitas procedentes de otras webs, con
+                las que el sitio web establece un contrato de afiliación (empresas de afiliación).
+            </li>
+            <li>
+                <b>Cookies de seguridad:</b> almacenan información cifrada para evitar que los datos guardados en ellas
+                sean vulnerables a ataques maliciosos de terceros.
+            </li>
+        </ul>
+    </div>
+
+    <div>
+        <p>SEGÚN LA PROPIEDAD</p>
+        <ul>
+            <li>
+                <b>Cookies propias:</b> son aquellas que se envían al equipo terminal del usuario desde un equipo o
+                dominio gestionado por el propio editor y desde el que se presta el servicio solicitado por el usuario.
+            </li>
+            <li>
+                <b>Cookies de terceros:</b> son aquellas que se envían al equipo terminal del usuario desde un equipo o
+                dominio que no es gestionado por el editor, sino por otra entidad que trata los datos obtenidos
+                través de las cookies.
+            </li>
+        </ul>
+    </div>
+
+    <div>
+        <p>SEGÚN EL PLAZO DE CONSERVACIÓN</p>
+        <ul>
+            <li>
+                <b>Cookies de sesión:</b> son un tipo de cookies diseñadas para recabar y almacenar datos mientras el
+                usuario accede a una página web.
+            </li>
+            <li>
+                <b>Cookies persistentes:</b> son un tipo de cookies en el que los datos siguen almacenados en el terminal
+                y pueden ser accedidos y tratados durante un período definido por el responsable de la cookie, y que
+                puede ir de unos minutos a varios años.
+            </li>
+        </ul>
+    </div>
+
+    <div>
+        <p><b>TRATAMIENTO DE DATOS PERSONALES</b></p>
+        <p>Sunpro Redes y Sistemas, s.l. es el <b> Responsable del tratamiento </b> de los datos personales del
+            <b> Interesado </b>y le informa de que estos datos serán tratados de conformidad con lo dispuesto en el
+            Reglamento (UE) 2016/679, de 27 de abril de 2016 (GDPR), por lo que se le facilita la siguiente información
+            del tratamiento:
+        </p>
+
+        <p>
+            <b>Fines del tratamiento:</b> según se especifica en el apartado de cookies que se utilizan en este sitio web.
+        </p>
+        <p>
+            <b>Legitimación del tratamiento:</b> salvo en los casos en los que resulte necesario para la navegación por la
+            web, por consentimiento del interesado (art. 6.1 GDPR).
+        </p>
+        <p>
+            <b>Criterios de conservación de los datos:</b> según se especifica en el apartado de cookies utilizadas en la
+            web.
+        </p>
+        <p>
+            <b>Comunicación de los datos:</b> no se comunicarán los datos a terceros, excepto en cookies propiedad de
+            terceros o por obligación legal.
+        </p>
+        <p><b>Derechos que asisten al Interesado:</b></p>
+        <ul>
+            <li>
+                Derecho a retirar el consentimiento en cualquier momento.
+            </li>
+            <li>
+                Derecho de acceso, rectificación, portabilidad y supresión de sus datos, y de limitación u oposición a su
+                tratamiento.
+            </li>
+            <li>
+                Derecho a presentar una reclamación ante la Autoridad de control (www.aepd.es) si considera que el
+                tratamiento no se ajusta a la normativa vigente.
+            </li>
+        </ul>
+
+        <p><b>Datos de contacto para ejercer sus derechos:</b></p>
+        <p>
+            Sunpro Redes y Sistemas, s.l.. Pol. Urtinsa, C/ Electrónica, nº 24 - 28923 Alcorcón (Madrid). <br>
+            E-mail: <a class="text-primary" href="mailto:contabilidad@sunprored.es">contabilidad@sunprored.es</a>
+        </p>
+    </div>
+</div>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.front.vertical', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\SPMiguel\resources\views/comun/front/cookies.blade.php ENDPATH**/ ?>
